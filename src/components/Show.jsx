0f64e057e66a039daacd0fe167841366533befcb
uvindu <randika.uvindu92@gmail.com>
2020-09-11 01:11:23 +1000

    API created/ working show page

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,39 +1,53 @@
-import React from "react"
-const Show = () => {
+import React, {useEffect, useContext} from "react"
+import Finder from "../apis/Finder"
+import { Context } from "../Context/Context"
+
+
+const Show = (props) => {
+   const{restaurants, setRestaurant} =useContext(Context);
+
+useEffect(() => {
+    const data = async () => {
+        try {
+            const response = await Finder.get("/");
+            setRestaurant(response.data.data.restaurants);
+          } catch (err) {
+              
+          }
+    };
+  
+    data();
+},[])
    return(
     <div className= "list group">
         <table className="table table-hover table-dark">
         <thead>
             <tr className="big-primary">
-                <th scope="call">Restaurants</th>
-                <th scope="call">Location</th>
-                <th scope="call">Price Range</th>
-                <th scope="call">Ratings</th>
-                <th scope="call">Edit</th>
-                <th scope="call">Delete</th>
+                <th scope="col">Restaurants</th>
+                <th scope="col">Location</th>
+                <th scope="col">Price Range</th>
+                <th scope="col">Ratings</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
 
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>McDonads</td>
-                <td>New York</td>
-                <td>$</td>
-                <td>Rating</td>
+           {restaurants.map(restaurant => {
+               return(
+                <tr key={restaurants.id}>
+                <td>{restaurant.name}</td>
+                <td>{restaurant.location}</td>
+                <td>{"$".repeat(restaurant.price_range)}</td>
+                <td>reveiw</td>
                 <td><button className="btn btn-warning">Update</button></td>
-                <td><button className="btn btn-danger">Delete</button></td>
+                 <td><button className="btn btn-danger">Delete</button></td>
 
             </tr>
+               )
 
-            <tr>
-                <td>McDonads</td>
-                <td>New York</td>
-                <td>$</td>
-                <td>Rating</td>
-                <td><button className="btn btn-warning">Update</button></td>
-                <td><button className="btn btn-danger">Delete</button></td>
-
-            </tr>
+             
+           })}
         </tbody>
 
         </table>
